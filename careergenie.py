719528f88b87c96133a6c7d2eca11640be4bc786
NisrokWu <113870208+NisrokWu@users.noreply.github.com>
2023-02-13 22:49:14 -0500

    careergenie

--- careergenie.py
+++ careergenie.py
@@ -1,7 +1,7 @@
 import openai
 import streamlit as st
 
-openai.api_key = st.secrets["API_Key"]
+openai.api_key = st.secrets["api_secret"]
 
 def generate_recommendations(prompt):
     completions = openai.Completion.create(
